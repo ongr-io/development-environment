dd63e3eb3464a22126517f377ece0d841b687354
Voldemaras Kadys <valdas@e-data.lt>
2015-01-12 16:33:14 +0200

    docker sample containers

--- .provision/puppet/manifests/default.pp
+++ .provision/puppet/manifests/default.pp
@@ -1,9 +1,30 @@
-package { "python-pip":
-  ensure => installed
+node /^vm\d+$/ {
+  package { "python-pip":
+    ensure => installed
+  }
+
+  package { "fig":
+    ensure   => latest,
+    provider => pip,
+    require  => Package["python-pip"]
+  }
 }
 
-package { "fig":
-  ensure   => latest,
-  provider => pip,
-  require  => Package["python-pip"]
+node /^nginx\d+$/ {
+  class { "nginx": }
+
+  #nginx::resource::vhost { "vm1.dev":
+  #  ensure      => present,
+  #  server_name => ["vm1.dev", "www.vm1.dev"],
+  #  index_files => ["app_dev.php"],
+  #  www_root    => "/srv",
+  #  try_files   => ['$uri', '$uri/', '/app_dev.php?$args']
+  #}
+
+  #nginx::resource::location { "root":
+  #  ensure   => present,
+  #  vhost    => "vm1.dev",
+  #  location => "/",
+  #  fastcgi  => "127.0.0.1:9000"
+  #}
 }
--- Vagrantfile
+++ Vagrantfile
@@ -7,12 +7,19 @@ Vagrant.configure( "2" ) do |config|
     v.name   = "vm1"
     v.cpus   = 1
     v.memory = 512
+    v.gui    = true
   end
 
+  config.vm.network :private_network, type: :dhcp
+  config.vm.synced_folder "./", "/srv/www", type: :nfs
+
+  config.hostsupdater.aliases = ["www.vm1.dev", "vm1.dev"]
+
   config.vm.provision :docker
-  config.vm.provision :shell, path: ".provision/provision"
+  config.vm.provision :shell, path: ".provision/bootstrap"
 
   config.vm.provision :puppet do |p|
+    p.module_path    = ".provision/puppet/modules"
     p.manifests_path = ".provision/puppet/manifests"
     p.options        = ["--verbose"]
   end
